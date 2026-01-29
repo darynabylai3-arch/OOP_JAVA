fa68bda8faef043cbed967b3b0dbb6e9231784bc
darn <darynabylai3@gmail.com>
2026-01-29 11:58:26 +0500

    updated task

--- lab1/task4.java
+++ lab1/task4.java
@@ -2,29 +2,35 @@ package lab1;
 
 import java.util.Scanner;
 
+import java.util.Scanner;
+
 public class task4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-	public static void main(String[] args) {
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        double c = sc.nextDouble();
 
-		Scanner scanner = new Scanner(System.in);
-		int a = scanner.nextInt();
-		int b = scanner.nextInt();
-		int c = scanner.nextInt();
-		
-		int D = b * b - 4 * a * c;
-		
-		if(D < 0) {
-			System.out.println("Error");
-		} 
-		if(D==0){
-			int x = (-b)/2*a;
-			System.out.print(x);
-		}
-		else System.out.println(D);
-        
-        scanner.close();
-		
-	}
+        double d = b * b - 4 * a * c;
 
+        if (d > 0) {
+            double x1 = (-b + Math.sqrt(d)) / (2 * a);
+            double x2 = (-b - Math.sqrt(d)) / (2 * a);
+            System.out.println(x1 + " " + x2);
+        } 
+        else if (d == 0) {
+            double x = -b / (2 * a);
+            System.out.println(x);
+        } 
+        else {
+            double real = -b / (2 * a);
+            double imag = Math.sqrt(-d) / (2 * a);
+            System.out.println(real + " + " + imag + "i");
+            System.out.println(real + " - " + imag + "i");
+        }
+    }
 }
+
+
 
